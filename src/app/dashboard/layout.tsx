f2dd6d820094a4045a2c3fd2168c2363b0359b4f
mohd jami <your_email@example.com>
2024-06-05 16:19:15 +0530

    Improved share playlist page

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -16,12 +16,19 @@ export default async function DashboardLayout({
   return (
     <div className="flex min-h-screen flex-col space-y-2">
       <Navbar />
-      <div className="container grid flex-1 gap-12 md:grid-cols-[200px_1fr] ">
-        <aside className="hidden w-[200px] flex-col md:flex mt-32">
-          <DashboardNav items={dashboardLinks.data} />
-        </aside>
-        <main className="flex w-full flex-1 flex-col mt-20">{children}</main>
-      </div>
+      {user ? (
+        <div className="container grid flex-1 gap-12 md:grid-cols-[200px_1fr] ">
+          <aside className="hidden w-[200px] flex-col md:flex mt-32">
+            <DashboardNav items={dashboardLinks.data} />
+          </aside>
+
+          <main className="flex w-full flex-1 flex-col mt-20">{children}</main>
+        </div>
+      ) : (
+        <div className="container grid flex-1 gap-12  ">
+          <main className="flex w-full flex-1 flex-col mt-20">{children}</main>
+        </div>
+      )}
     </div>
   );
 }
